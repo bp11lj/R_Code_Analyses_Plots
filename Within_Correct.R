824cc2942bade3f9fe325316f858a4d75a5064ab
Brent Pitchford <bp11lj@brocku.ca>
2022-05-02 08:55:59 -0400

    Update Within_Correct.R

--- Within_Correct.R
+++ Within_Correct.R
@@ -1,25 +1,26 @@
 #subject is subject column
-#between_sub_vars is list of between subject vars. Example -- c("explicit", "probability")
-#within_sub_vars is list of within subject vars. Example -- c("word_associat_size", "size_rew_font", "size_rew_shape", "StroopCond", "shaperewval", "block")
-#Accuracy_col is a column with whether trial is accurate or not. Accurate is 1 and incorrect is 0. Example -- 'correct'
+#between_sub_vars is list of between subject vars. Have in the dots
 #DV_col is a column with DV. Example -- 'rt'
 
-Within_Correct  <- function(dataset,  DV_col, ...) {
-DV_col <- enquo(DV_col)
+Within_Correct  <- function(dataset, subject, DV_col, ...) {
+  
+ subject <- enquo(subject)
 
   df <- dataset %>%
+    #group by between subject vars
   group_by(...) %>%
-  mutate(grndmeandv = mean(!!.DV_col, na.rm = T)) %>%
-  group_by(...) %>%
+  mutate(grndmeand = mean(dv, na.rm = T)) %>%
+    #group by ID
+  group_by(!!subject) %>%
 
   #average over conditions and get average per subj
-  mutate(condmean_perpardv = mean(!!.DV_col, na.rm = T)) %>%
+  mutate(condmean_perpar = mean(dv, na.rm = T)) %>%
 
   #make adjustment factor to add to all scores
-  mutate(adj_factdv = grndmeandv - condmean_perpardv) %>%
+  mutate(adj_fact = grndmeand - condmean_perpar) %>%
 
   ungroup()%>%
   #adjusted scores
-  mutate(dv_adj = dv + adj_factdv)
+  mutate(dv_adj = dv + adj_fact)
   return(df)
 }
